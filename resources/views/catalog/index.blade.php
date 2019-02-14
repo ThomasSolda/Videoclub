c7a336abff99c7a21416479ee0c0078c0d3c6ee8
ThomasASolda <thomassolda@icloud.com>
2019-02-13 23:30:32 -0300

    Vistas y rutas

---
+++ resources/views/catalog/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.master')
+
+@section('content')
+  Listado de películas 
+@stop
---
+++ resources/views/catalog/show.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.master')
+
+@section('content')
+  Vista detalle de película {{$id}} 
+@stop
